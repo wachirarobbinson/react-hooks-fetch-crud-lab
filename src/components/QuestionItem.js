0782b37d3671d68580f1b5a566b291bbc7d6f10e
wachirarobbinson <ngirigacharobbinson@gmail.com>
2023-08-03 13:44:26 +0300

    completed assignment

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,25 +1,30 @@
 import React from "react";
 
-function QuestionItem({ question }) {
-  const { id, prompt, answers, correctIndex } = question;
+function QuestionItem({ question, deleted, handleChange }) {
+	const { id, prompt, answers, correctIndex } = question;
 
-  const options = answers.map((answer, index) => (
-    <option key={index} value={index}>
-      {answer}
-    </option>
-  ));
+	const options = answers.map((answer, index) => (
+		<option key={index} value={index}>
+			{answer}
+		</option>
+	));
 
-  return (
-    <li>
-      <h4>Question {id}</h4>
-      <h5>Prompt: {prompt}</h5>
-      <label>
-        Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
-      </label>
-      <button>Delete Question</button>
-    </li>
-  );
+	return (
+		<li>
+			<h4>Question {id}</h4>
+			<h5>Prompt: {prompt}</h5>
+			<label>
+				Correct Answer:
+				<select
+					defaultValue={correctIndex}
+					onChange={(e) => handleChange(e, id)}
+				>
+					{options}
+				</select>
+			</label>
+			<button onClick={() => deleted(id)}>Delete Question</button>
+		</li>
+	);
 }
 
 export default QuestionItem;
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetch } from "whatwg-fetch";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
-  return (
-    <section>
-      <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
-  );
+	const [questions, setQuestions] = useState([]);
+
+	const FetchFn = () => {
+		fetch("http://localhost:4000/questions")
+			.then((res) => res.json())
+			.then((data) =>
+				setQuestions(
+					data.map((q) => {
+						return (
+							<QuestionItem
+								key={q.id}
+								handleChange={handleChange}
+								question={q}
+								deleted={deleted}
+							/>
+						);
+					})
+				)
+			);
+	};
+
+	useEffect(() => {
+		FetchFn();
+	}, []);
+
+	function deleted(id) {
+		fetch(`http://localhost:4000/questions/${id}`, { method: "DELETE" }).then(
+			() => FetchFn()
+		);
+	}
+
+	function handleChange(e, id) {
+		fetch(`http://localhost:4000/questions/${id}`, {
+			method: "PATCH",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				correctIndex: parseInt(e.target.value),
+			}),
+		});
+	}
+
+	return (
+		<section>
+			<h1>Quiz Questions</h1>
+			<ul>
+				{/* display QuestionItem components here after fetching */}
+				{questions}
+			</ul>
+		</section>
+	);
 }
 
 export default QuestionList;
